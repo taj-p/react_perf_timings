a0f283e7a2cdcc740748821367b177c6b0ef7900
Taj Pereira <taj@canva.com>
2023-03-07 11:40:03 +1030

    naive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [user, setUser] = React.useState(null)
+  React.useEffect(() => {
+    fetch('https://randomuser.me/api').then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error();
+      }
+    }).then(data => {
+      setUser(data)
+    }
+    )
+  }, [])
+
+  React.useEffect(() => {
+    if (user) {
+        performance.mark('RenderCompleteNaive');
+    }
+  }, [user]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <pre>
+        {JSON.stringify(user, null, 4)}
+      </pre>
     </div>
   );
 }
